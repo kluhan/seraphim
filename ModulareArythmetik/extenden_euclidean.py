a4d69ed3f8717cd39d3859886f73f13ade8866aa
nlensch <niclas.lensch@wpm-ingenieure.com>
2020-11-22 12:37:57 +0100

    refacotr euclide

--- ModulareArythmetik/extenden_euclidean.py
+++ ModulareArythmetik/extenden_euclidean.py
@@ -1,5 +1,7 @@
-def euclidean_forward(base,valueToInverse):
-    
+def getInverse(base,valueToInverse):
+    return __extendedEuklid(base,valueToInverse)
+
+def __extendedEuklid(base,valueToInverse):
     q = int(base / valueToInverse)
     if(q < 0):
         q = 0
@@ -29,20 +31,9 @@ def euclidean_forward(base,valueToInverse):
         x1 = invers
     
     return invers
+   
 
 
-print(euclidean_forward(26,13))
-
-print(euclidean_forward(26,15))
-print(euclidean_forward(26,5))
-print(euclidean_forward(26,19))
-print(euclidean_forward(26,17))
-print(euclidean_forward(26,27))
-print(euclidean_forward(26,80))
-print(euclidean_forward(26,610))
-print(euclidean_forward(26,5007))
-print(euclidean_forward(26,850))
-print(euclidean_forward(26,8))
 
 
 
