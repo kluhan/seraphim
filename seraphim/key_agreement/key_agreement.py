1b2d0fac65922777770e218fee1902643faa3bc4
Klaus Luhan <klaus.luhan@gmail.com>
2021-03-31 03:45:05 +0200

    CurveBenchmark

--- seraphim/key_agreement/key_agreement.py
+++ seraphim/key_agreement/key_agreement.py
@@ -14,7 +14,7 @@ from seraphim.elliptic_curves.elliptic_curve_point import (
 
 
 class KeyAgreement:
-    def __init__(self, domain, style="projective", v=True):
+    def __init__(self, domain="curve25519", style="projective", v=True):
         self.secret = None
         self.local_key = None
         self.shared_key = None
